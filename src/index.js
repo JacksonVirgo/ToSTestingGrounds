bfeea891683a540bd1435d7893aa68a51683b5c9
Jack Nash <jacknash42@gmail.com>
2022-05-31 03:50:28 +1000

    mobile design prototype

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Game from './pages/Game'
+import './styles/global.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <Game/>
+  </React.StrictMode>
+);
---
+++ src/pages/Game.jsx
@@ -0,0 +1,41 @@
+import styles from '../styles/game.module.css';
+import LastWillImage from '../res/icon_last_will.png';
+import DeathNote from '../res/icon_death_note.png';
+import Icon from '../res/icon_action.png';
+import SendIcon from '../res/send.png'
+
+const ChatMessage = ({ author, message }) => {
+    return <div className={styles.gameChatMessage}>
+        <strong>{author}</strong> {message}
+    </div>
+}
+
+const ChatBar = () => {
+    return <form onSubmit={(e)=>{e.preventDefault()}}>
+
+    </form>
+}
+
+export default () => {
+    return <div className={styles.gameRoot}>
+        <div className={styles.gameMain}>
+            <div className={styles.gamePhase}>Day 1 - 0:42</div>
+            <div className={styles.gameChat}>
+                <ChatMessage author='NashedPotato' message='Message Content' />
+                <ChatMessage author='NashedPotato' message='Message Content' />
+                <ChatMessage author='NashedPotato' message='Message Content' />
+            </div>
+            <div className={styles.gameActionBar}>
+                <div>👥</div>
+                <img src={Icon}/>
+                <img src={DeathNote}/>
+                <img src={LastWillImage}/>
+                <div>🔈</div>
+            </div>
+            <div className={styles.gameChatBar}>
+                <input type='text' className={styles.gameChatInput} />
+                <img className={styles.gameChatSend} src={SendIcon}/>
+            </div>
+        </div>
+    </div>
+}
